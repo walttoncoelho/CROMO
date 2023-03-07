1dd595e86bbfe822abca23e0ab04390e9cd8bbb3
walttoncoelho <walltton@outlook.com>
2023-03-07 19:53:41 -0300

    Ajuste menu mobile

--- cromo/src/pages/Home/Home.jsx
+++ cromo/src/pages/Home/Home.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
 import Footer from '../../components/footer/Footer'
-import MenuMobile from '../../components/MenuMobile/MenuMobile'
 
 import Navbar from '../../components/Navbar/Navbar'
-
 
 
 export default function Home() {
   return (
     <>
   <Navbar />
-  <MenuMobile/>
+
   
     <div>Home</div>
 <Footer />
