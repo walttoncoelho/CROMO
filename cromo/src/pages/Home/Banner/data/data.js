4ad8f6e6c51094f14e835eb2758de8008837f834
walttoncoelho <walltton@outlook.com>
2023-04-16 18:33:43 -0300

    Adiciona o Mock da Seção Banner

---
+++ cromo/src/pages/Home/Banner/data/data.js
@@ -0,0 +1,18 @@
+const data = [
+    {
+  "id": 123,
+  "title": "Banner de exemplo",
+  "imageUrl": "https://example.com/banner.jpg",
+  "linkUrl": "https://example.com/",
+  "altText": "Este é um banner de exemplo",
+  "dimensions": {
+    "width": 728,
+    "height": 90
+  },
+  "startDate": "2023-05-01",
+  "endDate": "2023-05-31",
+  "isActive": true
+}
+
+];
+export default data;
--- cromo/src/routes/AppRoutes.jsx
+++ cromo/src/routes/AppRoutes.jsx
@@ -17,8 +17,7 @@ export function AppRoutes() {
       <Route path="/sobre" element={<Sobre />} />
       <Route path="/empreendimentos" element={<Empreendimentos />} />
       <Route path="/faleconosco" element={<FaleConosco />} />
-      <Route path="/manager" element={<Login />} />
-
+      <Route path="/manager" element={<Login />} />      
       <Route path="/templates" element={<Templates />} />
 
       {/* Páginas auxiliares */}
