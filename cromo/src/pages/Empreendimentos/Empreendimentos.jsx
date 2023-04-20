116c88d5f8bde0cfa0aadf29bd66e5f4260fa2ec
walttoncoelho <walltton@outlook.com>
2023-04-19 23:43:29 -0300

    Inicia a página empreendimentos

--- cromo/src/pages/Empreendimentos/Empreendimentos.jsx
+++ cromo/src/pages/Empreendimentos/Empreendimentos.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
+import Footer from '../../components/footer/Footer'
+import SectionNumeros from '../Home/SectionNumeros/SectionNumeros'
 
 export default function Empreendimentos() {
   return (
     <div>
 <>
-Empreendimentos
+<Navbar/>
+<h1>Nossos empreendimentos</h1>
+<SectionNumeros/>
+<Footer/>
 </>
     </div>
   )
