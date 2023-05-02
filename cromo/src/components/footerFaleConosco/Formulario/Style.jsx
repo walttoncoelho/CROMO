045b5958a722cefeda525f42cabed7ac34e5a284
walttoncoelho <walltton@outlook.com>
2023-05-01 23:57:32 -0300

    Style Page Fale conosco Mapa

--- cromo/src/components/footerFaleConosco/Formulario/Style.jsx
+++ cromo/src/components/footerFaleConosco/Formulario/Style.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const FormContainer = styled.form`
 background-color: #ffffff;
 height: 100%;
-height: 550px;
+height: 480px;
 
 @media (max-width: 550px) {
   height: 620px;
@@ -13,7 +13,6 @@ height: 550px;
 width: 400px;
 border-radius: 16px;
 padding: 32px;
-gap: 18px;
 box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
 `;
 
@@ -25,7 +24,6 @@ justify-content: center;
 `;
 
 export const Radio = styled.div`
-
 @media (max-width: 550px) {
   display: flex;
   margin: 30px;
@@ -41,7 +39,6 @@ font-weight: 400px;
 
 
 export const Input = styled.input`  
-
 @media (max-width: 550px) {
   display: flex;
   align-items: center;
@@ -49,12 +46,11 @@ export const Input = styled.input`
   margin: auto;
   max-width: 300px;  
 }
-
     width: 100%;   
     min-width: 300px;
     height: 48px;
     border-radius: 8px;
-    padding: 10px;
+    padding: 2px;
     margin-bottom: 10px;
     border-style: none;
     background-color: #F1F1F1;
@@ -82,7 +78,7 @@ export const InputArea = styled.input`
 
 height: 148px;
 border-radius: 8px;
-padding: 10px;
+padding: 2px;
 min-width: 400px;
 margin-bottom: 10px;
 border-style: none;
@@ -92,8 +88,6 @@ font-weight: 500;
     &:focus {
     outline: 2px solid #06aacf;
   }
-
-
     `;
 
 export const Button = styled.button`
--- cromo/src/components/footerFaleConosco/Mapa/MapaFooter.jsx
+++ cromo/src/components/footerFaleConosco/Mapa/MapaFooter.jsx
@@ -1,22 +1,35 @@
 import React from 'react'
+import Mapa from "./img/MAPA.png";
+import { ColunaMapa, MapaDescription } from './Style';
+import { Container } from './Style';
+import Arrow from "./img/arrow.svg";
 
 
 export default function MapaFooter() {
   return (
     <>
-    
-      <div>
-        <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3985.543961463451!2d-44.27009014909125!3d-2.6523775393818667!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7f6831f936054e3%3A0xd44bbfd57ba32587!2sJCAmbiental!5e0!3m2!1spt-BR!2sbr!4v1650202108897!5m2!1spt-BR!2sbr"
-            
-                allowfullscreen=""
-                loading="lazy"
-                referrerpolicy="no-referrer-when-downgrade"
-                title="Mapa"
-              ></iframe>
-      </div>
+      <Container>
 
-   
+        <ColunaMapa>
+          <a href="/">
+            <img src={Mapa} alt="logo" />
+          </a>
+        </ColunaMapa>
+        <MapaDescription>
+          <p>
+            Visite nosso escritório em Balsas na Rua Vitória N32, Barro XYZ
+          </p>
+          <a
+            href="https://www.google.com/maps/place/Balsas,+MA,+65800-000/@-7.5206636,-46.0530322,15z/data=!4m6!3m5!1s0x92d5ef9625fb9fa5:0x697ed5cde68bbfe7!8m2!3d-7.5335065!4d-46.0406425!16s%2Fg%2F11bxftrzcq"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            Conhecer
+            <img src={Arrow} alt="Arrow" />
+          </a>
+        </MapaDescription>
+      </Container>
     </>
   )
 }
