68336d43e5bbef210dc6fb28a264b2cc908ad6c1
ciroDourado <ciro.brz@gmail.com>
2023-05-07 22:47:39 -0300

    feat: integra big numbers ao site

--- cromo/src/pages/Home/SectionNumeros/SectionNumeros.jsx
+++ cromo/src/pages/Home/SectionNumeros/SectionNumeros.jsx
@@ -1,25 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import NumerosBloco from "../../../components/Numeros/NumerosBloco";
 import BigNumeros from "./BigNumeros";
 import { BlocoDesc, BlocoNumbers, ContainerNumeros } from "./Style";
 
-
+import api from "../../../services/api";
 
 export default function Numeros() {
+  let [numerosIniciais, setNumerosIniciais] = useState(null);
 
+  useEffect(() => {
+    async function obterNumerosIniciais() {
+      let { data: numeros } = await api.get("/numeros");
+      setNumerosIniciais(numeros);
+    };
+    obterNumerosIniciais();
+  }, []);
 
   return (
     <>
-    <ContainerNumeros>
-      <BlocoDesc>
-
-    <BigNumeros/>
-      </BlocoDesc>
-      <BlocoNumbers>
-    <NumerosBloco />
-      </BlocoNumbers>
-    </ContainerNumeros>
+      <ContainerNumeros>
+        <BlocoDesc>
+          { numerosIniciais && <BigNumeros detalhes={ numerosIniciais } />}
+        </BlocoDesc>
+        <BlocoNumbers>
+          { numerosIniciais && <NumerosBloco numerosIniciais={ numerosIniciais } />}
+        </BlocoNumbers>
+      </ContainerNumeros>
     </>
   );
 }
