7c41e796d0ad7c14fc05b20a36a6bfea0a4f54e0
ciroDourado <ciro.brz@gmail.com>
2023-05-07 23:34:59 -0300

    fix: adequa mock ao schema de numeros da api

--- cromo/src/data/componentNumeros/numeros.js
+++ cromo/src/data/componentNumeros/numeros.js
@@ -1,14 +1,11 @@
-const numeros = [
-    {
-  "id": 1,
-  "title": "Nossos Big Numbers",  
-  "descricao": "Lorem ipsum dolor sit amet consectetur. Mauris ultrices aliquam justo id cras. Tristique elit orci vitae tellus at massa amet. Etiam quis tincidunt non nisl mi nisl pretium consectetur pretium. Sit nulla scelerisque varius volutpat nam a quam bibendum. Tellus vehicula tortor ac commodo. Ac venenatis a potenti pellentesque tincidunt lacus. Cursus tristique ultricies consequat elementum morbi pulvinar.", 
+const numeros = {
+  id: 1,
+  titulo: "Nossos Big Numbers",  
+  descricao: "Lorem ipsum dolor sit amet consectetur. Mauris ultrices aliquam justo id cras. Tristique elit orci vitae tellus at massa amet. Etiam quis tincidunt non nisl mi nisl pretium consectetur pretium. Sit nulla scelerisque varius volutpat nam a quam bibendum. Tellus vehicula tortor ac commodo. Ac venenatis a potenti pellentesque tincidunt lacus. Cursus tristique ultricies consequat elementum morbi pulvinar.", 
 
-  "lotes": "+1.550",   
-  "asfalto": "5.000km", 
-  "rua": "+500", 
-  "familias": "1.550", 
-},
-
-];
+  rua: 500, 
+  lotes: 1550,   
+  asfalto: 5000, 
+  familias: 1550, 
+};
 export default numeros;
