8862aa0466d2a2ec32358911e4dec1fade54fe71
ciroDourado <ciro.brz@gmail.com>
2023-05-16 22:10:09 -0300

    feat: formulário de criação para empreendimentos

--- cromo/src/pages/Manager/Conteudo/Empreendimentos/CriaEmpreendimentos/CriaEmpreendimentos.jsx
+++ cromo/src/pages/Manager/Conteudo/Empreendimentos/CriaEmpreendimentos/CriaEmpreendimentos.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import NavBar from '../../../components/NavBar/NavBar'
 import AdministrativePanel from '../../../components/AdministrativePanel/AdministrativePanel'
 import { Container, ContainerSidebar, ContainerStage } from './Style/Style'
+import api from "../../../../../services/api"
 
 
 export default function CriaEmpreendimentos() {
+  let [opcoes, setOpcoes] = useState(null);
+
+  useEffect(() => {
+    async function obterOpcoes() {
+      let { data } = await api.get("/manager/empreendimento/create-options", {
+        headers: { "Authorization": `Bearer ${localStorage.getItem("token")}` }
+      });
+      setOpcoes(data);
+    };
+    obterOpcoes().catch(error => setOpcoes({
+      statusDaConstrucao: [],
+      infraestruturas: [],
+    }));
+  });
+  let handleSubmit = console.log
   return (
     <>
     <NavBar />
@@ -13,12 +29,37 @@ export default function CriaEmpreendimentos() {
         <AdministrativePanel />
       </ContainerSidebar>
       <ContainerStage>
-        <br />
-        <h3>Painel - Conteúdo - Vovo Empreendimentos</h3>
-        <br />
-        <hr />
-        <br />
-    
+        <form>
+          <label for="status">Em exibição</label>
+          <input value="false" type="checkbox" id="status" name="status" />
+
+          <fieldset>
+            <legend>Status da Construção</legend>
+
+            {opcoes?.statusDaConstrucao.map(status => (
+              <React.Fragment key={status.valor}>
+                <input type="radio" name="statusDaConstrucao" value={status.valor} />
+                <label>{status.texto}</label>
+              </React.Fragment>
+            ))}
+          </fieldset>
+
+          <fieldset>
+            <legend>Infraestruturas</legend>
+
+            {opcoes?.infraestruturas.map(infraestrutura => (
+              <React.Fragment key={infraestrutura.id} >
+                <img src={`http://localhost:3000/infraestruturas/${infraestrutura.id}/icone`} />
+                <input type="checkbox" name="infraestruturas" value={infraestrutura.id} />
+                <label>{infraestrutura.titulo}</label>
+              </React.Fragment>
+            ))}
+          </fieldset>
+
+          <button type="button" onClick={handleSubmit} disabled={false}>
+            Salvar
+          </button>
+        </form>
       </ContainerStage>
     </Container>
   </>
