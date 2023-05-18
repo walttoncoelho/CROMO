6f1c789b47a4ae913a3dc027e5e72159c344a7a1
ciroDourado <ciro.brz@gmail.com>
2023-05-17 23:01:47 -0300

    fix: normaliza dados hardcoded de acordo com a API

--- cromo/src/pages/Home/Empreendimentos/data/data.jsx
+++ cromo/src/pages/Home/Empreendimentos/data/data.jsx
@@ -1,42 +1,39 @@
 const data = [
-    {
-  "id": 1,
-  "title": "Residencial Florença 1 Etapa",
-  "descricao":"Lotes comerciais & residenciais",
-  "imageUrl": "../images/empreendimentos/florenca01.jpg",
-  "linkUrl": "/empreendimentoId",
-  "altText": "Lotes comerciais & residenciais",
-  "status": "Em contrução",
-  "startDate": "2023-05-01",
-  "endDate": "2023-05-31",
-  "isActive": true,
-
-},
-{
-  "id": 2,
-  "title": "Residencial Florença 2 Etapa",
-  "descricao":"Lotes comerciais & residenciais",
-  "imageUrl": "../images/empreendimentos/florenca02.jpg",
-  "linkUrl": "empreendimentoId2",
-  "altText": "Lotes comerciais & residenciais",
-    "status": "Em contrução",
-  "startDate": "2023-05-01",
-  "endDate": "2023-05-31",
-  "isActive": true,
-
-},
-{
-  "id": 3,
-  "title": "Residencial Veneza",
-  "descricao":"Lotes comerciais & residenciais",
-  "imageUrl": "../images/empreendimentos/veneza01.png",
-  "linkUrl": "/",
-  "altText": "Lotes comerciais & residenciais",
-    "status": "Obra entregue",
-  "startDate": "2023-05-01",
-  "endDate": "2023-05-31",
-  "isActive": true
-}
-
+  {
+    "id": 1,
+    "titulo": "Residencial Florença 1 Etapa",
+    "tipoEmpreendimento": "Lotes comerciais & residenciais",
+    "logoEmpreendimento": "../images/empreendimentos/florenca01.jpg",
+    "slug": "empreendimentoId",
+    "altText": "Lotes comerciais & residenciais",
+    "statusDaConstrucao": "Em contrução",
+    "startDate": "2023-05-01",
+    "endDate": "2023-05-31",
+    "status": true,
+  },
+  {
+    "id": 2,
+    "titulo": "Residencial Florença 2 Etapa",
+    "tipoEmpreendimento": "Lotes comerciais & residenciais",
+    "logoEmpreendimento": "../images/empreendimentos/florenca02.jpg",
+    "slug": "empreendimentoId2",
+    "altText": "Lotes comerciais & residenciais",
+    "statusDaConstrucao": "Em contrução",
+    "startDate": "2023-05-01",
+    "endDate": "2023-05-31",
+    "status": true,
+  },
+  {
+    "id": 3,
+    "titulo": "Residencial Veneza",
+    "tipoEmpreendimento": "Lotes comerciais & residenciais",
+    "logoEmpreendimento": "../images/empreendimentos/veneza01.png",
+    "slug": "/empreendimentoId3",
+    "altText": "Lotes comerciais & residenciais",
+    "statusDaConstrucao": "Obra entregue",
+    "startDate": "2023-05-01",
+    "endDate": "2023-05-31",
+    "status": true
+  }
 ];
 export default data;
