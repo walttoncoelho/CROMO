8f4420c03d7ff861f6641e2b4256a2dfcb8e5dd0
ciroDourado <ciro.brz@gmail.com>
2023-05-18 22:05:04 -0300

    fix: corrige valores padrao de bignumbers

--- cromo/src/data/componentNumeros/numeros.js
+++ cromo/src/data/componentNumeros/numeros.js
@@ -3,10 +3,10 @@ const numeros = {
   titulo: "Nossos Big Numbers",  
   descricao: "Em mais de 30 anos de história, a Cromo vem ajudando milhares de balsenses a realizar o sonho de seu lote ou casa própria e contribuindo no desenvolvimento do Sul do Maranhão..", 
 
-  lotes: 0,
-  asfalto: 0,
-  rua: 0,
-  familias: 0,
+  lotes: 1200,
+  asfalto: 2600,
+  rua: 140,
+  familias: 700,
 }
 
 export default numeros;
