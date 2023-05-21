855be6db70af286ba1e865b30875174557eec41d
ciroDourado <ciro.brz@gmail.com>
2023-05-21 17:18:41 -0300

    feat: integra entre em contato com back

--- cromo/src/components/footerFaleConosco/Footer.jsx
+++ cromo/src/components/footerFaleConosco/Footer.jsx
@@ -10,32 +10,29 @@ import MapaFooter from "./Mapa/MapaFooter";
 export default function FooterFaleConosco() {
   return (
     <>
-    
-      <Container>
-        <SectionFaleconosco>
+    <Container>
+      <SectionFaleconosco>
         <Contatos/>
-          <ColunaFormulario>   
-          <h2>
-            Ente em contato            
-          </h2>    
+        <ColunaFormulario>   
+          <h2>Ente em contato</h2>    
           <br/>    
-            <FooterFormulario />         
-          </ColunaFormulario>
-          <ColunaMapa>
+          <FooterFormulario />         
+        </ColunaFormulario>
+        <ColunaMapa>
           <MapaFooter/>
-          </ColunaMapa>
-        </SectionFaleconosco>
-        <ByGoodApp>
-          <p>
-            ©2023 Cromo Construtora e Incorporadora | Todos os direitos
-            reservados{" "}
-            <a href="https://www.goodapp.com.br"
-              target="_blank"
-              rel="noopener noreferrer"
-            > </a>
-          </p>
-        </ByGoodApp>
-      </Container>
+        </ColunaMapa>
+      </SectionFaleconosco>
+      <ByGoodApp>
+        <p>
+          ©2023 Cromo Construtora e Incorporadora | Todos os direitos
+          reservados{" "}
+          <a href="https://www.goodapp.com.br"
+            target="_blank"
+            rel="noopener noreferrer"
+          > </a>
+        </p>
+      </ByGoodApp>
+    </Container>
     </>
   );
 }
