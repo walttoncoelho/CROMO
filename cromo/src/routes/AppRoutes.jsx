f9beec6ec534a19d97e3742d4b9cd1aa3d5c91af
walttoncoelho <walltton@outlook.com>
2023-07-05 10:40:04 -0300

    corrige mensagem de contato3

--- cromo/src/routes/AppRoutes.jsx
+++ cromo/src/routes/AppRoutes.jsx
@@ -1,25 +1,6 @@
 import React from "react";
 import { Routes, Route,  Redirect } from "react-router-dom";
 
-import Sobre from "../pages/Sobre/Sobre";
-import FaleConosco from "../pages/FaleConosco/FaleConosco";
-
-import Painel from "../pages/Manager/Painel/Painel/Painel";
-import Templates from "../pages/Templates/Templates";
-import Empreendimentos from "../pages/Empreendimentos/SectionEmpreend";
-import Perfil from "../pages/Manager/Painel/Perfil/Perfil";
-import Servicos from "../pages/Manager/Painel/Servicos/Servicos";
-import Configuracoes from "../pages/Manager/Painel/Configuracoes/Configuracoes";
-import Suporte from "../pages/Manager/Painel/Suporte/Suporte";
-import ListaBanners from "../pages/Manager/Conteudo/Banner/ListaBanner/ListaBanner";
-import CriaBanner from "../pages/Manager/Conteudo/Banner/CriaBanner/CriaBanner";
-import CriaEmpreendimentos from "../pages/Manager/Conteudo/Empreendimentos/CriaEmpreendimentos/CriaEmpreendimentos";
-import ListaEmpreendimentos from "../pages/Manager/Conteudo/Empreendimentos/ListaEmpreendimentos/ListaEmpreendimentos";
-import EmpreendimentoInterna from "../pages/EmpreendimentosInterna/EmpreendimentoInterna";
-import EditaNumeros from "../pages/Manager/Conteudo/Numeros/EditaNumeros/EditaNumeros";
-import CriaInfraestruturas from "../pages/Manager/Conteudo/Infraestruturas/CriaInfraestruturas/CriaInfraestruturas";
-import ListaInfraestruturas from "../pages/Manager/Conteudo/Infraestruturas/ListaInfraestruturas/ListaInfraestruturas";
-import ProtectedRoute from "./ProtectedRoute";
 
 export function AppRoutes() {
  
@@ -27,31 +8,9 @@ export function AppRoutes() {
   return (
     /* Páginas públicas */
     <Routes>
- 
-     
-      <Route path="/*" element={<Redirect to="https://cromoconstrutora.com.br/"/>}/>
-      
-      <Route path="/sobre" element={<Sobre />} />
-      <Route path="/empreendimentos" element={<Empreendimentos />} />
-      <Route path="/empreendimentos/:empreendimentoSlug" element={<EmpreendimentoInterna />} />
-      <Route path="/faleconosco" element={<FaleConosco />} />
-      <Route path="/templates" element={<Templates />} />
 
-      {/* Páginas privadas */}
-      <Route path="/manager/painel" element={<ProtectedRoute><Painel /></ProtectedRoute>} />
-      <Route path="/manager/perfil" element={<ProtectedRoute><Perfil /></ProtectedRoute>} />
-      <Route path="/manager/servicos" element={<ProtectedRoute><Servicos /></ProtectedRoute>} />
-      <Route path="/manager/Configuracoes" element={<ProtectedRoute><Configuracoes /></ProtectedRoute>} />
-      <Route path="/manager/Suporte" element={<ProtectedRoute><Suporte /></ProtectedRoute>} />
+      <Route path="/" element={<Redirect to="https://cromoconstrutora.com.br/"/>}/>      
 
-      {/* Conteúdos*/}
-      <Route path="/manager/bannerlist" element={<ProtectedRoute><ListaBanners /></ProtectedRoute>} />
-      <Route path="/manager/bannercreate" element={<ProtectedRoute><CriaBanner /></ProtectedRoute>} />
-      <Route path="/manager/empreendimentolist" element={<ProtectedRoute><ListaEmpreendimentos /></ProtectedRoute>} />
-      <Route path="/manager/empreendimentocreate" element={<ProtectedRoute><CriaEmpreendimentos /></ProtectedRoute>} />
-      <Route path="/manager/infraestruturalist" element={<ProtectedRoute><ListaInfraestruturas /></ProtectedRoute>} />
-      <Route path="/manager/infraestruturacreate" element={<ProtectedRoute><CriaInfraestruturas /></ProtectedRoute>} />
-      <Route path="/manager/numeroedit" element={<ProtectedRoute><EditaNumeros /></ProtectedRoute>} />
 
     </Routes>
   );
